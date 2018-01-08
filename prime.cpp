a770302ecd8652f8e996a7902cc8c91efbcacb4a
priyasoundh <32993324+priyadharshiniap@users.noreply.github.com>
2018-01-08 21:24:16 +0530

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,24 @@
+#include<iostream.h>
+void main()
+{
+int n,c=o,i;
+cout<<"enter the num";
+cin<<n;
+for(i=0i<n;i++)
+{
+if(n%i==0)
+{
+c++;
+}
+if(c==2)
+{
+cout<<"the number is prime";
+}
+else
+{
+cout<<"the number is not prime";
+}
+}
+}
+
+    © 2018 GitH
