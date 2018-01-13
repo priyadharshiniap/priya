f21a1280c9b4a89d4c56c22ed0e276a885dfec72
priyasoundh <32993324+priyadharshiniap@users.noreply.github.com>
2018-01-13 15:38:44 +0530

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c,d;
+  cout<<"enter a number";
+  cin>>a;
+  b=a%10;
+  a=a/10;
+  c=a%10;
+  a=a/10;
+  d=1%10;
+  cout<<b<<c<<d;
+  }
