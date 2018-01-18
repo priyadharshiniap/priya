30bb8e1bf28061c175b79c0eb51071320143228f
priyasoundh <32993324+priyadharshiniap@users.noreply.github.com>
2018-01-18 09:45:45 +0530

    Create holiday.cpp

---
+++ holiday.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+voidmain();
+{
+  char a;
+  clrscr();
+  cout<<"enter a day";
+  cin>>a;
+  if(a=='saturday'||a=='sunday')
+  {
+    cout<<"holiday";
+  }
+  else
+  cout<<"not a holiday";
+  getch();
+}
