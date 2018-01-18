cc0b1c814d3e9825dd2b60beb62ee57055686399
priyasoundh <32993324+priyadharshiniap@users.noreply.github.com>
2018-01-18 10:14:27 +0530

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,12 @@
+#include<iostream.H>
+#include<conio.h>
+void main()
+{
+  int b,h,a;
+  clrscr();
+  cout<<"enter the base and height of a triangle";
+  cin>>b>>h
+  a=(b*h)/2
+  cout<<"area is"<<a;
+  getch();
+}
