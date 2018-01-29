efc67ddbf919fcd152bceb8c3bee9fb1d9fc5e90
priyasoundh <32993324+priyadharshiniap@users.noreply.github.com>
2018-01-29 21:09:36 +0530

    Create digits.cpp

---
+++ digits.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int a,b=0;
+  cout<<"enter a number";
+  cin>>a;
+  do
+  {
+    if(a/10=!0)
+    {
+      a=a/10;
+      b=b+1;
+      
+    }
+  }
+  while(a/10=!0)
+  cout<<b;
+  getch();
+  }
