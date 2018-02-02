beea9264c6a10b511e1adafe578a11db1391e6b2
priyasoundh <32993324+priyadharshiniap@users.noreply.github.com>
2018-02-02 15:34:26 +0530

    Create smallarray.cpp

---
+++ smallarray.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int a[100],n,i,b;
+  clrscr();
+  cout<<"enter no of elements";
+  cin>>n;
+  cout<<"enter array elements";
+  for(i=0;i<n;i++)
+  {
+    cin<<a[i];
+  }
+  for(i=0;i<n;i++)
+  {
+    
