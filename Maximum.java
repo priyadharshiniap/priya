fa15383aa986d2abb017869473f55ffc12399af2
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-08-07 13:51:04 +0530

    Create Maximum.java

---
+++ Maximum.java
@@ -0,0 +1,27 @@
+import java.util.*r;
+class Maximum
+{
+	public static void main(String args[])
+	{
+		int a[]=new int[100];
+		int min,n;
+		Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("enter values"+i);
+			a[i]=sc.nextInt();
+		}
+		min=a[0];
+		for(int j=1;j<n;j++)
+		{
+			if(min<a[j])
+			{
+				min=a[j];
+			}
+				
+		}
+		System.out.println("minimum is"+min);
+		sc.close();
+	}
+}
