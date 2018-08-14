c7c316570d33223b8d8035f4ee99442738d100a8
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-08-14 14:03:11 +0530

    Create Sort.java

---
+++ Sort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Sort
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int i,j,m,n,k,t;
+    int a[]=new int[100];
+    System.out.println("enter the value of n");
+    n=sc.nextInt();
+    System.out.println("enter "+n+" values");
+    for(i=0;i<n;i++)
+    {
+      a[i]=sc.nextInt();
+    }
+    for(j=0;j<n;j++)
+    {
+    for(m=j+1;m<n;m++)
+    {
+      if(a[j]>a[m])
+      {      
+        t=a[m];
+        a[m]=a[j];
+        a[j]=t;
+      }
+    }
+  }
+  System.out.println("sorting elements are");
+  for(k=0;k<n;k++)
+  {
+    System.out.println(a[k]);
+  }
+  }
+}
