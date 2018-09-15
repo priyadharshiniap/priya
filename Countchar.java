2fd63e8611608ab6f17f1eaaced02aa990ca845d
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-09-15 22:34:08 +0530

    Update Countchar.java

--- Countchar.java
+++ Countchar.java
@@ -2,17 +2,16 @@ class CountChar
 {
     static void count(String str)
     {
-        // Create an char array of given String
         char[] ch = str.toCharArray();
         for (int i = 0; i < ch.length; i++) {
  
-            // Declare an String with empty initialization
+      
             String s = "";
  
-            // When the character is not space
+            
             while (i < ch.length && ch[i] != ' ') {
  
-                // concat with the declared String
+               
                 s = s + ch[i];
                 i++;
             }
