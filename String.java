07521676650790f25872745104391da0012bf6fb
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-09-16 21:00:26 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class String
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner();
+    string s=sc.next(System.in);
+    int k=sc.nextInt(System.in);
+    for(int i=0;i<k;i++)
+    {
+      System.out.println(s);
+    }
+  }
+}
