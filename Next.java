874b974af85d66636da10d299db61a613da84760
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-03 21:16:57 +0530

    Create Next.java

---
+++ Next.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Next
+{
+  public static void main(String args[])
+  {
+    int a;
+    Scanner sc=new Scanner(System.in);
+    a=sc.nextInt();
+    System.out.println(a+1);
+  }
+}
