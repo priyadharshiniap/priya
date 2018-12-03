6f51a3ff7820d56fc791ada651b41d8f25f600c6
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-03 21:43:20 +0530

    Create Sumk.java

---
+++ Sumk.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Sumk
+{
+  public static void main(String args[])
+  {
+    int a,c=0,i;
+    Scanner sc=new Scanner(System.in);
+    a=sc.nextInt();
+    for(i=1;i<=a;i++)
+    {
+      c=c+i;
+    }
+    System.out.println(c);
+  }
+}
