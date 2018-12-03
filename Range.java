bf4e1856105110f294d41848d813cd933d1366a4
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-03 21:12:39 +0530

    Update Range.java

--- Range.java
+++ Range.java
@@ -4,14 +4,15 @@ class Range
   public static void main(String args[])
   {
     int a;
-    Scanner sc=new scanner(System.in);
-    if(a>=1&&a<=10)
-    {
-      System.out.println("yes");
-    }
-    else
-    {
-      System.out.println("no");
-    }
+    	Scanner sc=new Scanner(System.in);
+    	a=sc.nextInt();
+    	if(a>=1&&a<=10)
+    	{
+    		System.out.println("yes");
+    	}
+    	else
+    	{
+    		System.out.println("no");
+    	}
   }
 }
