7e96ef2b4d0073dec00c6a85904aa9b9d41b4dd0
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-03 21:36:40 +0530

    Create Prodevodd.java

---
+++ Prodevodd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Prodevodd
+{
+  public static void main(String args[])
+  {
+    int a,b,c;
+    Scanner sc=new Scanner(System.in);
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=a*b;
+    if(c%2==0)
+    {
+      System.out.println("even");
+    }
+    else
+    {
+      System.out.println("odd");
+    }
+  }
+}
+© 2018 GitHub, Inc.
