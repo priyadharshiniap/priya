7021f4f4bd0292e88c1da97b8bf69c64089a1db5
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-03 21:20:27 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -3,10 +3,10 @@ class Arithmetic
 {
   public static void main(String args[])
   {
-    Scanner sc=new Scanner();
-    int n=sc.nextInt(System.in);
-    int a=sc.nextInt(System.in);
-    int d=sc.nextInt(System.in);
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int a=sc.nextInt();
+    int d=sc.nextInt();
     int ap=((n/2)*((2*a)+((n-1)*d)));
     System.out.println(ap);
   }
