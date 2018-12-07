cc2d2473e884ea93b0168328db004a44a20d6d10
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-07 16:20:39 +0530

    Update Countchar.java

--- Countchar.java
+++ Countchar.java
@@ -1,30 +1,21 @@
 import java.util.*;
-class CountChar
+public class Countchar
 {
-    static void count(String str)
-    {
-        char[] ch = str.toCharArray();
-        for (int i = 0; i < ch.length; i++) {
- 
-      
-            String s = "";
- 
-            
-            while (i < ch.length && ch[i] != ' ') {
- 
-               
-                s = s + ch[i];
-                i++;
-            }
- 
-            if (s.length() > 0) 
-                System.out.println(s + "->" + s.length());            
-        }
-    }
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        String str = sc.next();
-        count(str);
+        String str ="guvi codekata beginner";
+        int n=str.length();
+        int count=0;
+        char ch[]=str.toCharArray();
+        for(int j=0;j<n;j++)
+        {
+            count=count+1;
+            if(ch[j]==' ')
+            {
+                j++;
+            }
+        }
+        System.out.println(str+"--->"+count);
     }
 }
