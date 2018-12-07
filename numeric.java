da3aa7b7cd5cdd604c69a54a738530bc60b9a703
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-07 10:59:12 +0530

    Create numeric.java

---
+++ numeric.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String []args)
+    {
+        int i,a=0;
+        Scanner sc=new Scanner(System.in);
+        String ss=sc.next();
+        int n=ss.length();
+        char[] s=new char[n];
+        for(int j=0;j<n;j++)
+        {
+            s[j]=ss.charAt(j);
+        }
+        for(i=0;i<n;i++)
+        {
+            if((s[i]>='a')&&(s[i]<='z'))
+            {
+                a=a+1;
+            }
+        }
+        if(a==0)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+     }
+}
