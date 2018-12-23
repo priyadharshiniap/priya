a49351aca3793159061b6024c6c8036fc001f87d
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-23 12:21:39 +0530

    Create Lowmax.java

---
+++ Lowmax.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Lowmax
+{
+	public static void main(String[] args)
+	{
+	    int n,i,min,max;
+	    Scanner sc=new Scanner(System.in);
+	    n=sc.nextInt();
+	    int a[]=new int[n];
+	    for(i=0;i<n;i++)
+	    {
+	        a[i]=sc.nextInt();
+	    }
+	    min=a[0];
+	    max=a[0];
+	    for(i=1;i<n;i++)
+	    {
+	        if(a[i]<min)
+	        {
+	            min=a[i];
+	        }
+	    }
+	    for(i=1;i<n;i++)
+	    {
+	        if(a[i]>max)
+	        {
+	            max=a[i];
+	        }
+	    }
+		System.out.println(min+" "+max);
+	}
+}
