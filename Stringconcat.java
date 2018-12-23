89a4fce316f64296da6c3b64e9d4f231639e8bcf
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-23 12:52:31 +0530

    Update and rename String.java to Stringconcat.java

---
+++ Stringconcat.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Stringconcat
+{
+	public static void main(String[] args)
+	{
+	    String a,b,c;
+	    Scanner sc=new Scanner(System.in);
+	    a=sc.next();
+	    b=sc.next();
+	    c=a+b;
+		System.out.println(c);
+	}
+}
