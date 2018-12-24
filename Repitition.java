ec1df089fad4782903fd720cf72655fff1572b8a
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-24 20:15:32 +0530

    Create Repitition.java

---
+++ Repitition.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Repitition
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int i,count=0;
+	    int a=sc.nextInt();
+	    int b=sc.nextInt();
+	    int e[]=new int[a];
+	    for(i=0;i<a;i++)
+	    {
+	        e[i]=sc.nextInt();
+	        if(e[i]==b)
+	        {
+	            count=count+1;
+	        }
+	    }
+	    System.out.println(count);
+	}
+}
