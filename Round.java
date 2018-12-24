c74bbb2899460dec4ee6e008823d665ca080c119
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-24 22:26:39 +0530

    Create Round.java

---
+++ Round.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Round
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a;
+        a=sc.nextDouble();
+        int c=((int) a)+1;
+        System.out.println(c);
+    }
+}
