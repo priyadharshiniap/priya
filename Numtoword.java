058edc3c3d78e1a0b63394b1a68bc8b5abac67fb
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-24 20:25:21 +0530

    Create Numtoword.java

---
+++ Numtoword.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.lang.*;
+public class Numtoword
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int i,j=0;
+	    int a=sc.nextInt();
+	    if(a==1)
+	    {
+	        System.out.print("one");
+	    }
+	    if(a==2)
+	    {
+	        System.out.print("two");
+	    }
+	    if(a==3)
+	    {
+	        System.out.print("three");
+	    }
+	    if(a==4)
+	    {
+	        System.out.print("four");
+	    }
+	    if(a==5)
+	    {
+	        System.out.print("five");
+	    }
+	    if(a==6)
+	    {
+	        System.out.print("six");
+	    }
+	    if(a==7)
+	    {
+	        System.out.print("seven");
+	    }
+	    if(a==8)
+	    {
+	        System.out.print("eight");
+	    }
+	    if(a==9)
+	    {
+	        System.out.print("nine");
+	    }
+	    if(a==10)
+	    {
+	        System.out.print("ten");
+	    }
+	}
+}
