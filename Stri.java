f6dcb7d6c8c4cc095015be9ed63b31dfbdaa01c9
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-24 20:41:23 +0530

    Create Stri.java

---
+++ Stri.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Stri
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String str ="priyadharshini";
+        int a=sc.nextInt();
+        char ch[]=str.toCharArray();
+        for(int j=0;j<a;j++)
+        {
+            System.out.print(ch[j]);
+        }
+    }
+}
