2713d18860c5b02cbade3cc46d1ef7cba10bb242
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-24 21:50:56 +0530

    Create String1.java

---
+++ String1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+public class String1
+{
+	public static void main(String args[])
+	{
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        if(System.out.printf(s) == null)
+        {
+        
+        }
+	}
+}
