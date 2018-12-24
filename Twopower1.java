3ff909928364bf2adac7a9f7b2bfdb323bf2999b
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-24 21:23:01 +0530

    Update and rename Twopower.java to Twopower1.java

---
+++ Twopower1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+public class Twopower1
+{
+	public static void main(String args[])
+	{
+		int a,b,c,i=1;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		while(i<=a)
+		{
+		    i=2*i;
+		}
+		System.out.println(i);
+	}
+}
