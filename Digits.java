0adba7ae21177dca16cc39ed0521675ea5c668b4
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2018-12-24 19:52:49 +0530

    Create Digits.java

---
+++ Digits.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+public class Digits
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int i,j;
+	    int a=sc.nextInt();
+	    double b=Math.log10(a);
+	    int c=((int) b)+1;
+	    int e[]=new int[c];
+	    for(i=c-1;i>=0;i--)
+	    {
+	        e[i]=a%10;
+	        a=a/10;
+	    }
+	    for(j=0;j<c;j++)
+	    {
+	        System.out.print(e[j]+" ");
+	    }
+	}
+}
