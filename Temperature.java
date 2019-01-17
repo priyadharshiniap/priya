f5075ce1f443334c9be5772c17569fdf6272b444
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-01-17 10:38:57 +0530

    Create Temperature.java

---
+++ Temperature.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Temperature
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int k=n+273;
+		System.out.println(k);
+	}
+}
