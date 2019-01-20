b1c22d6670d81c0de6b97d4fceae6f96f05065f8
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-01-20 22:00:15 +0530

    Create Kabali.java

---
+++ Kabali.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Kabali
+{
+	public static void main(String[] args)
+	{
+	    Scanner read = new Scanner(System.in);
+	    while(read.hasNext())
+	    {
+	        int a=read.nextInt();
+	        int b=read.nextInt();
+	        int c=b-a;
+		    System.out.println(c);
+	    }
+	}
+}
