94f61797703c70f232f9e2ac4eb29d5a02a91cc1
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-01-20 22:44:46 +0530

    Create Intconcat.java

---
+++ Intconcat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Intconcat
+{
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        String a=""+n+k;
+        System.out.println(a);
+     }
+}
