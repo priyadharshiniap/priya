4b968db1028c43e6152af3c685f6c72feb341a22
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-02-03 23:35:04 +0530

    Update Multi.java

--- Multi.java
+++ Multi.java
@@ -1,13 +1,15 @@
 import java.util.*;
-public class Multi
+import java.lang.*;
+import java.io.*;
+class Multi
 {
-    public static void main(String []args)
-    {
-        Scanner sc=new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
         int c=sc.nextInt();
         int d=(a*b)/c;
         System.out.println(d);
-     }
+	}
 }
