1928925cace32c57cc932fcf1f4ca5479f44ce75
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-02-04 13:54:46 +0530

    Update power.cpp

--- power.cpp
+++ power.cpp
@@ -1,17 +1,14 @@
-#include<iostream.h>
-#include<conio.h>
-void main()
+#include<iostream>
+using namespace std;
+int main()
 {
   int num,power,i; 
-  clrscr();
-  cout<<"enter the number";
   cin>>num;
-  cout<<"enter the power";
   cin>>power;
   for(i=1;i<=power;i++)
   {
     power=power*num;
   }
   cout<<power;
-  getch();
+  return 0;
 }
