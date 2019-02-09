b286c55159363c0ff73b79f620a870ee4e51513e
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-02-09 20:40:44 +0530

    Update Median.java

--- Median.java
+++ Median.java
@@ -9,15 +9,11 @@ class Median
   
 		Scanner sc=new Scanner(System.in);
  
-		 int i,n,k;
+		 int i,n,k,j,m,t;
  
 		 int a[]=new int[100];
 
-		System.out.println("enter the value of n");
  		n=sc.nextInt();
-  
-		System.out.println("enter "+n+" values");
-  
 		for(i=0;i<n;i++)
  
  		{
@@ -25,7 +21,18 @@ class Median
 			a[i]=sc.nextInt();
  
 		 }
-  
+  		for(j=0;j<n;j++)
+    {
+    for(m=j+1;m<n;m++)
+    {
+      if(a[j]>a[m])
+      {      
+        t=a[m];
+        a[m]=a[j];
+        a[j]=t;
+      }
+    }
+  }
 		 if(n%2==0)
 		{
 			k=(n-1)/2;
@@ -34,6 +41,6 @@ class Median
 		{
 			k=(n)/2;	
 		}
-		System.out.println("median is"+a[k]);
+		System.out.println(a[k]);
 	}
 }
