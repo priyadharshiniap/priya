0002df35fe56514a5586172fa5265d785812a6fc
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-02-11 14:46:26 +0530

    Update Index.java

--- Index.java
+++ Index.java
@@ -1,22 +1,23 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 class Index
 {
-	public static void main(String args[])
+	public static void main (String[] args) throws java.lang.Exception
 	{
 		int a[]=new int[100];
 		int n;
 		Scanner sc=new Scanner(System.in);
-    n=sc.nextInt();
+    		n=sc.nextInt();
 		for(int i=0;i<n;i++)
 		{
-			System.out.println("enter values"+i);
 			a[i]=sc.nextInt();
 		}
-		for(int j=1;j<n;j++)
+		for(int j=0;j<n;j++)
 		{
 		  	System.out.println(a[j]+" "+j);	
 		}
-    System.out.println();
+    		System.out.println();
 		sc.close();
 	}
 }
