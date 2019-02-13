c736f2094d17b80af2bd1fa164563916cfb77442
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-02-13 14:35:55 +0530

    Update Time.java

--- Time.java
+++ Time.java
@@ -1,14 +1,14 @@
 import java.util.*;
-public class Time
+import java.lang.*;
+import java.io.*:
+class Time
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws java.lang.Exception
 	{
         int a,b,c,d,e,f;
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter time1");
         a=sc.nextInt();
         b=sc.nextInt();
-        System.out.println("enter time2");
         c=sc.nextInt();
         d=sc.nextInt();
         e=(a*60)+b;
@@ -23,6 +23,6 @@ public class Time
         }
         b=e/60;
         c=e%60;
-        System.out.println(b+"hours"+c+"minutes");
+        System.out.println(b+" "+c);
 	}
 }
