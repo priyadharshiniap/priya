3173b731610c20b1a9dfcdcc438d050e3a514c0a
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-02-21 10:45:52 +0530

    Update Countline.java

--- Countline.java
+++ Countline.java
@@ -6,21 +6,21 @@ class Countline
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-		int count=1;
-		while(sc.hasNextLine())
-		{
-        	String str =sc.next();
-        	int n=str.length();
-        	char ch[]=str.toCharArray();
-        	for(int j=0;j<n;j++)
-        	{
+	int count=1;
+	while(sc.hasNextLine())
+	{
+            String str =sc.next();
+            int n=str.length();
+            char ch[]=str.toCharArray();
+            for(int j=0;j<n;j++)
+            {
             	if(ch[j]=='.')
             	{
                 	j++;
                 	count=count+1;
             	}
         	}
-		}
+	}
         System.out.println(count);
     }
 }
