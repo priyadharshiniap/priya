458f7449b178ff487b793dc4efc3f51b6c079fe6
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-03-04 14:02:26 +0530

    Update Countspecial.java

--- Countspecial.java
+++ Countspecial.java
@@ -1,26 +1,24 @@
 import java.util.*;
-public class Countspecial
+class Countspecial
 {
-	public static void main(String[] args)
-	{
+    public static void main(String[] args)
+    {
         Scanner sc=new Scanner(System.in);
-        String str ="gu3vi._2codek@ta 1beginner.";
-        int n=str.length();
         int count=0;
-        char ch[]=str.toCharArray();
-        for(int j=0;j<n;j++)
-        {
-            if((ch[j]>='a'&&ch[j]<='z')||ch[j]==' '||(ch[j]>='0'&&ch[j]<='9'))
-            {
-                //j++;
-                //count=count+1;
-            }
-            else
-            {
-                j++;
-                count=count+1;
-            }
-        }
-        System.out.println(str+"--->"+count);
-	}
+        while(sc.hasNextLine())
+		{
+			String str =sc.next();
+        		int n=str.length();
+        		char ch[]=str.toCharArray();
+        		for(int j=0;j<n;j++)
+        		{
+            			if((ch[j]>='a'&&ch[j]<='z')||ch[j]==' '||(ch[j]>='0'&&ch[j]<='9'));
+            			else
+            			{
+                			count++;
+            			}
+        		}
+		}
+        System.out.println(count);
+    }
 }
