f66ccaff928125b597f20333240825137c1b3520
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-03-04 13:56:29 +0530

    Update Countnumber.java

--- Countnumber.java
+++ Countnumber.java
@@ -1,26 +1,23 @@
 import java.util.*;
-public class Countnumber
+class Countnumber
 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        String str ="gu3vi. 2codekata 1beginner.";
-        int n=str.length();
         int count=0;
-        char ch[]=str.toCharArray();
-        for(int j=0;j<n;j++)
-        {
-            if((ch[j]>='a'&&ch[j]<='z')||ch[j]=='.'||ch[j]==' ')
-            {
-                //j++;
-                //count=count+1;
+        while(sc.hasNextLine())
+		{
+			String str =sc.next();
+        	int n=str.length();
+        	char ch[]=str.toCharArray();
+        	for(int j=0;j<n;j++)
+        	{
+            	if(ch[j]>='0'&&ch[j]<='9')
+            	{
+                	count=count+1;
+            	}
             }
-            else
-            {
-                j++;
-                count=count+1;
-            }
-        }
-        System.out.println(str+"--->"+count);
+		}
+        System.out.println(count);
     }
 }
