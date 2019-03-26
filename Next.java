5d6e54d0c0d97a8473664f3a135d90a6dae7a82f
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-03-26 21:04:17 +0530

    Update Next.java

--- Next.java
+++ Next.java
@@ -1,11 +1,13 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 class Next
 {
-  public static void main(String args[])
-  {
-    int a;
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a;
     Scanner sc=new Scanner(System.in);
     a=sc.nextInt();
     System.out.println(a+1);
-  }
+	}
 }
