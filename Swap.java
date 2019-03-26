a1f8ea69390c46ed71ce1cb28db88df6661c9841
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-03-26 19:01:52 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 class Swap
 {
-  public static void main(String args[])
-  {
-    Scanner sc=new Scanner(System.in);
-    int a=sc.nextInt();
-    int b=sc.nextInt();
-    int t;
-    t=a;
-    a=b;
-    b=t;
-    System.out.println(a+" "+b);
-  }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+    	int a=sc.nextInt();
+	    int b=sc.nextInt();
+	    int t;
+	    t=a;
+	    a=b;
+	    b=t;
+	    System.out.println(a+" "+b);
+	}
 }
