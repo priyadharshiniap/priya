b000d7dab2983cb775a1d4af309d05f976574ecb
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-03-26 21:14:03 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,12 +1,14 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 class Sum
 {
-  public static void main(String args[])
-  {
-    int a,b;
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b;
     Scanner sc=new Scanner(System.in);
     a=sc.nextInt();
     b=sc.nextInt();
     System.out.println(a+b);
-  }
+	}
 }
