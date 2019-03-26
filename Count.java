53a28d124eaa0c303c056df9906f2a3a48f7a03f
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-03-26 20:48:22 +0530

    Update Count.java

--- Count.java
+++ Count.java
@@ -1,9 +1,11 @@
 import java.util.*;
-public class Count
+import java.lang.*;
+import java.io.*;
+class Count
 {
-	public static void main(String[] args)
+	public static void main (String[] args) throws java.lang.Exception
 	{
-	    int n,a,count=0;
+		int n,a,count=0;
 	    Scanner sc=new Scanner(System.in);
 	    n=sc.nextInt();
 	    while(n!=0)
