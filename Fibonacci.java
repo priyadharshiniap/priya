550b4883aee8d1ff8199fd68c904bca29f6e6c30
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-03-26 19:19:37 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,16 +1,19 @@
 import java.util.*;
-public class Fibonacci
+import java.lang.*;
+import java.io.*;
+class Fibonacci
 {
-	public static void main(String[] args)
+	public static void main (String[] args) throws java.lang.Exception
 	{
-	    Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
 		int a=sc.nextInt();
-		int b=0;
+		int b=1;
 		int c=1,d;
-		for(int i=0;i<a;i++)
+		System.out.print(b);
+		for(int i=1;i<a;i++)
 		{
 		    d=c+b;
-		    System.out.println(c);
+		    System.out.print(" "+c);
 		    b=c;
 		    c=d;
 		}
