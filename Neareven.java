10eed4c7d8170b78d69bed1fbea8d7790e5e3ac0
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-03-27 20:50:16 +0530

    Update Neareven.java

--- Neareven.java
+++ Neareven.java
@@ -1,18 +1,20 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 class Neareven
 {
-  public static void main(String args[])
-  {
-    int a;
-    Scanner sc=new Scanner(System.in);
-    a=sc.nextInt();
-    if(a%2==0)
-    {
-      System.out.println(a);
-    }
-    else
-    {
-      System.out.println(a-1);
-    }
-  }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a;
+	    Scanner sc=new Scanner(System.in);
+	    a=sc.nextInt();
+	    if(a%2==0)
+	    {
+	      System.out.println(a);
+	    }
+	    else
+	    {
+	      System.out.println(a-1);
+	    }
+	}
 }
