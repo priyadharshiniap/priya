b7545b73ce184b377130219f67fcb4ddfbd43a6b
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-03-27 21:42:03 +0530

    Update Sumk.java

--- Sumk.java
+++ Sumk.java
@@ -1,15 +1,17 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 class Sumk
 {
-  public static void main(String args[])
-  {
-    int a,c=0,i;
-    Scanner sc=new Scanner(System.in);
-    a=sc.nextInt();
-    for(i=1;i<=a;i++)
-    {
-      c=c+i;
-    }
-    System.out.println(c);
-  }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,c=0,i;
+	    Scanner sc=new Scanner(System.in);
+	    a=sc.nextInt();
+	    for(i=1;i<=a;i++)
+	    {
+	      c=c+i;
+	    }
+	    System.out.println(c);
+	}
 }
