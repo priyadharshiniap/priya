513e1c0d2f6b30799bcbf3976a496223980f4b76
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-03-28 20:25:26 +0530

    Update Stri.java

--- Stri.java
+++ Stri.java
@@ -1,15 +1,17 @@
 import java.util.*;
-public class Stri
+import java.lang.*;
+import java.io.*;
+class Stri
 {
-    public static void main(String[] args)
-    {
-        Scanner sc=new Scanner(System.in);
-        String str ="priyadharshini";
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        String str =sc.next();
         int a=sc.nextInt();
         char ch[]=str.toCharArray();
         for(int j=0;j<a;j++)
         {
             System.out.print(ch[j]);
         }
-    }
+	}
 }
