0fa96cca99cf5713a3d1f880738c230256828508
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-04-02 20:43:46 +0530

    Update Stri.java

--- Stri.java
+++ Stri.java
@@ -6,12 +6,12 @@ class Stri
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-        	String str =sc.next();
-        	int a=sc.nextInt();
-        	char ch[]=str.toCharArray();
-        	for(int j=0;j<a;j++)
-        	{
-            		System.out.print(ch[j]);
-        	}
+		String str =sc.next();
+		int a=sc.nextInt();
+		char ch[]=str.toCharArray();
+		for(int j=0;j<a;j++)
+		{
+			System.out.print(ch[j]);
+		}
 	}
 }
